fc7878871793caa7f665e4c639e184a9007aa5d6
benjaminchew95 <benjaminchew95@gmail.com>
2017-06-24 10:37:00 +0800

    ben

--- Orbital1417/pairing/views.py
+++ Orbital1417/pairing/views.py
@@ -20,19 +20,45 @@ from userAcc.forms import SignUpForm
 from django.contrib.auth.decorators import login_required
 
 from pairing.models import Pairing
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 # main pairing page to allow charity to post their job listing
 def pairingPost(request):
 
-    post = Pairing.objects.all()
+    #post = Pairing.objects.all()
 
-    query = request.GET.get('q')
-    if query:
-        post = post.filter(title__icontains=query)
+    #query = request.GET.get('q')
+    #if query:
+        #post = post.filter(title__icontains=query)
+
+    #context = {
+    #'post':post
+    #}
+
+    post_list = Pairing.objects.all()
+    paginator = Paginator(post_list, 1) # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        post = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        post = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        post = paginator.page(paginator.num_pages)
 
     context = {
     'post':post
     }
+
+    query = request.GET.get('q')
+    if query:
+        post_list = post_list.filter(title__icontains=query)
+        context ={'post':post_list
+        }
+
     return render(request, 'pairingPage.html', context)
 
 @login_required
