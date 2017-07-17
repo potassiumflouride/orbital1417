b8ae0949a14c742dc83c17b8128058b850070521
benjaminchew95 <benjaminchew95@gmail.com>
2017-07-17 13:00:12 +0800

    updated search fun

--- Orbital1417/tracklah/views.py
+++ Orbital1417/tracklah/views.py
@@ -18,15 +18,30 @@ def index(request):
         # If page is out of range (e.g. 9999), deliver last page of results.
         post = paginator.page(paginator.num_pages)
 
-    context = {
-    'post':post
-    }
 
-    query = request.GET.get('q')
+
+    """query = request.GET.get('q')
     if query:
         post_list = post_list.filter(title__icontains=query) #change here to change search value type
         context ={'post':post_list,
         'q':query
-        }
+        }"""
+
+    context = {
+    'post':None,
+    }
+
+    query =  request.GET.get('q')
+    if request.method == 'GET' and query: # this will be GET now
+        try:
+            post1 = CharPost.objects.filter(title__icontains=query)
+        except:
+            None # filter returns a list so you might consider skip except part
+        if post1!=post_list:
+            return render(request,'trackhome.html',{"post":post1})
+        else:
+            return render(request,'trackhome.html',{"post":False})
+    else:
+        return render(request,'trackhome.html',{"post":None,})
 
     return render(request, 'trackhome.html', context)
