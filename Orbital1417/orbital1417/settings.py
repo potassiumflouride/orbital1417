e4ef77f7533edda1e7445c8bbc868cd97cff3233
benjaminchew95 <benjaminchew95@gmail.com>
2017-08-21 14:02:58 +0800

    more or less done

--- Orbital1417/orbital1417/settings.py
+++ Orbital1417/orbital1417/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'livereload',
+
 ]
 
 MIDDLEWARE = [
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
+
 
 ]
 
