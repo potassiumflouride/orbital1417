54f27e395844ca1c2181969cd91a63504ab23ac1
potassiumflouride <potassiumflouride@gmail.com>
2017-08-22 21:21:51 +0800

    testingPythonAnywhere

--- Orbital1417/orbital1417/settings.py
+++ Orbital1417/orbital1417/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 #heroku
 """import dj_database_rul
 db_from_env=dj_database_rul.config*conn_max_age=500)
