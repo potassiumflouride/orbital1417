dab02f3d485936fb31c32f2777cd7380dac86426
potassiumflouride <potassiumflouride@gmail.com>
2017-08-22 18:07:43 +0800

    heroku testing

--- Orbital1417/orbital1417/settings.py
+++ Orbital1417/orbital1417/settings.py
@@ -126,7 +126,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+#heroku
+import dj_database_rul
+db_from_env=dj_database_rul.config*conn_max_age=500)
+DATABASE['default'].update(db_from_env)
+
+STATIC_RUL= '/static/'
+STATICFILES_DIRS= [
+    os.path.join(BASE_DIR,"static"),
+
+]
+#heroku end
+
+STATIC_ROOT- os.path.join( BASE_DIR,"staticfiles")
+STATIFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static"),
