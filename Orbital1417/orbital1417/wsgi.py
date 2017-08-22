c69ed7d252f5d65dcc2995ed31877ff5eabc02c6
potassiumflouride <potassiumflouride@gmail.com>
2017-08-22 21:29:30 +0800

    testingPythonAnywhere

--- Orbital1417/orbital1417/wsgi.py
+++ Orbital1417/orbital1417/wsgi.py
@@ -8,7 +8,20 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+path = '/home/HuhY/orbital1417/Orbital1417'
+
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ [ 'DJANGO_SETTINGS_MODULE']="website.settings"
+
+from django.core.wsgi import get_wsgi_application
+application= get_wsgi_application()
+
+
+'''
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orbital1417.settings")
@@ -17,3 +30,4 @@ application = get_wsgi_application()
 
 from whitenoise.django import DjangoWhiteNoise
 application= DjangoWhiteNoise(application)
+'''
